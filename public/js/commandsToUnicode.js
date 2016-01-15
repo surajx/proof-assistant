7bbeaced0d76d35c61c5b619164c67d7f62e3030
Boris <u5844485@anu.edu.au>
2016-01-15 21:38:33 +1100

    Added dynamic text command to unicode to formule and select rule as well, refactored code as well

--- public/js/commandsToUnicode.js
+++ public/js/commandsToUnicode.js
@@ -1,4 +1,18 @@
 // replace LaTex with unicode
+
+function dynamicTextFormInput() {
+		 var formInput = $(this).val();
+         var prevCaretPos = this.selectionStart;
+         var origProofNameLen = formInput.length;
+         formInput = commandsToUnicode(formInput);
+         curCaretOffset = origProofNameLen - formInput.length;
+           if (curCaretOffset > 0){
+             $(this).val(formInput);
+             this.setSelectionRange(prevCaretPos-curCaretOffset, prevCaretPos-curCaretOffset);
+             }
+
+}
+
 function commandsToUnicode(formInput) {
     
     formInput = formInput
