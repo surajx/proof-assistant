82b104bce12e6d4c62597cb124d2c0ae1032b55e
Boris <u5844485@anu.edu.au>
2016-01-16 20:56:45 +1100

    removed for loop, fixed minor typos in error messages

--- FOL/proof/rules/DisjunctionIntroRule.js
+++ FOL/proof/rules/DisjunctionIntroRule.js
@@ -3,20 +3,19 @@ var getTopLevelFormulasForConnective = require('./ruleUtil.js').getTopLevelFormu
 var primeFormulaForCompare = require('./ruleUtil.js').primeFormulaForCompare;
 
 function DisjunctionIntroRule(){
-  DisjunctionIntroRule.rulename = "";
 }
 
 DisjunctionIntroRule.prototype.validate = function(proofGraph, curProofLine){
   var lnoErrStr = "[line: "+curProofLine.lineNo+"]: "
   var rulePremises = proofGraph.getAdjOf(curProofLine);
   if(curProofLine.annotations.length!==1) {
-    throw lnoErrStr + "∨I rule should have two annotations. Given: " + curProofLine.annotationsStr.join(',');
+    throw lnoErrStr + "∨I rule should have only one annotation. Given: " + curProofLine.annotationsStr.join(',');
   }
   if(rulePremises.length!==1) {
-    throw lnoErrStr + "∨I rule should have exactly two premises. Make sure that your \
-      annotation contains references to exactly two line-numbers.";
+    throw lnoErrStr + "∨I rule should have exactly one premise. Make sure that your \
+      annotation references exactly one line-number.";
   }
-  console.log("executed", rulePremises, curProofLine)
+  
   var dependencyVerifyObj = dependencyVerifier(rulePremises, curProofLine);
 
   if (!dependencyVerifyObj.status){
@@ -31,13 +30,12 @@ DisjunctionIntroRule.prototype.validate = function(proofGraph, curProofLine){
     throw lnoErrStr + "Could not find a top level conjunction for the given formule, check your \
     formule and make sure that an ∨ symbol is the outermost logical connective."
   }
-  for (var i = rulePremises.length - 1; i >= 0; i--) {
-    var tmpFormule = primeFormulaForCompare(rulePremises[i].formule);
-    if (topLevelConjuncts.indexOf(tmpFormule)<0){
-      throw lnoErrStr + "A formule specified in the annotation is not a \
-        top level conjunct in the proof line."
+  
+  var tmpFormule = primeFormulaForCompare(rulePremises[0].formule);
+  if (topLevelConjuncts.indexOf(tmpFormule)<0){
+    throw lnoErrStr + "A formule specified in the annotation is not a \
+    top level conjunct in the proof line."
     }
-  }
   return true;
 }
 
