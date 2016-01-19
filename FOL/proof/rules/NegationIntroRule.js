e61d271fb899126764b16db98dd488b7a54fe49c
Boris <u5844485@anu.edu.au>
2016-01-19 16:44:27 +1100

    added vacuous discharge to neg intro, removed unnecessary checks

--- FOL/proof/rules/NegationIntroRule.js
+++ FOL/proof/rules/NegationIntroRule.js
@@ -13,9 +13,9 @@ NegationIntroRule.prototype.validate = function(proofGraph, curProofLine){
     throw lnoErrStr + "¬I rule should have only one annotation. Given: " + curProofLine.annotationsStr.join(',');
   }
   
-  if(rulePremises.length!==2) {
-    throw lnoErrStr + "¬I rule should have exactly two premises. Make sure that your \
-      annotation contains references to exactly two line-numbers.";
+  if(rulePremises.length < 1 && 2 < rulePremises.length) {
+    throw lnoErrStr + "¬I rule should have one premise. Make sure that your \
+      annotation contains references to one or more line-numbers.";
   }
   
   var dependencyVerifyObj = dependencyVerifier(rulePremises, curProofLine);
@@ -28,30 +28,32 @@ NegationIntroRule.prototype.validate = function(proofGraph, curProofLine){
     }
   }
   
-  //Check that rulePremises is correctly sorted
-  if (curProofLine.annotations[0].annotation != rulePremises[0].lineNo){
-    rulePremises.reverse();
-  }
-  
-  //Check that rulePremise lines are correct (this may be redundant)
-  if (curProofLine.annotations[0].annotation != rulePremises[0].lineNo 
-      && curProofLine.annotations[0].discharge != rulePremises[1].lineNo){
-    throw lnoErrStr + "lines referenced by given annotations do not match retrieved \
-      lines"
+  // Check for vacuous discharge  
+  if (curProofLine.annotations[0].discharge === ''){
+    var tmpCurForm = primeFormulaForCompare(curProofLine.formule);
+    if (tmpCurForm.charAt(0)!== '¬'){
+      throw lnoErrStr + "Inputted formulae does not have a negation"
+    }
+  } else {
+    //Check that rulePremise lines are correct (sanity check)
+    if (curProofLine.annotations[0].annotation != rulePremises[0].lineNo 
+        && curProofLine.annotations[0].discharge != rulePremises[1].lineNo){
+      throw lnoErrStr + "lines referenced by given annotations do not match retrieved \
+        lines"
+    }
+    //Check that current formulae correctly adds ¬ to formulae of line discharged
+    var tmpCurForm = primeFormulaForCompare(curProofLine.formule);
+    var tmpRefForm = primeFormulaForCompare(rulePremises[1].formule);
+    if(tmpCurForm !== "¬" + tmpRefForm){
+      throw lnoErrStr + "discharged formulae does not match the negated input formulae";
+    }
   }
   
   //Check that formulae referenced by annotation only contains ⊥
   if(rulePremises[0].formule !== "⊥"){
-    throw lnoErrStr + "¬I rule annotation can only referance the formule \'⊥\'";
+    throw lnoErrStr + "¬I rule annotation can only reference the formulae \'⊥\'";
   }
   
-  //Check that current formulae correctly adds ¬ to formulae of line discharged
-  var tmpCurForm = primeFormulaForCompare(curProofLine.formule);
-  var tmpRefForm = primeFormulaForCompare(rulePremises[1].formule);
-  if(tmpCurForm !== "¬" + tmpRefForm){
-    throw lnoErrStr + "discharged formulae does not match the negated inputed formulae";
-  }
-
   return true;
 }
 
