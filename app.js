9ef19665cde0c0ebbbf662902bc53898c368d294
Suraj Narayanan S <u5881495@anu.edu.au>
2016-01-21 20:37:17 +1100

    Quick Fix for payload limit

--- app.js
+++ app.js
@@ -26,10 +26,8 @@ app.set('view engine', 'jade');
 
 app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(session({
   cookieName: 'session',
   secret: config.session.secret,
