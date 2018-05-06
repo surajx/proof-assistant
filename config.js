c963a0b8d60b24746ed64684d8bd252ee0996996
Liming Zheng <u5923899@anu.edu.au>
2018-05-06 14:21:48 +1000

    localhost verification my mac

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
       45bf6cbeb58c9e6737b369319ab0a5fd19'
   },
   recaptcha: {
-    siteKey: process.env.RECAPTCHA_SITE_KEY || '6Lc5VzQUAAAAAPfATvrGumvCp6XHNnNvPKRAsmf0',
-    secretKey: process.env.RECAPTCHA_SITE_SECRET || 'siteSecret'
+    siteKey: process.env.RECAPTCHA_SITE_KEY || '6LdqglcUAAAAABbiAE6F_5cJD1cscFDSV7UUOMO4',//6Lc5VzQUAAAAAPfATvrGumvCp6XHNnNvPKRAsmf0
+    secretKey: process.env.RECAPTCHA_SITE_SECRET || '6LdqglcUAAAAAEIg-iLb083-LKfzkVAoBa5O10EP'//,siteSecret
   }
 };
